863f4a7d57552a062bad4c980cc5f5df91096fdd
Marco Terzer <terzerm@gmail.com>
2015-02-26 01:27:23 +0100

    added ThreadLocals class to allow resetting of all ThreadLocal variables (fix)

--- src/main/java/org/decimal4j/arithmetic/ThreadLocals.java
+++ src/main/java/org/decimal4j/arithmetic/ThreadLocals.java
@@ -1,20 +1,21 @@
 package org.decimal4j.arithmetic;
 
 /**
- * Helper class to reset all thread locals to {@code null}. This may be useful
- * if the library is used in a web service or servlet container.
+ * Helper class to remove all values held by {@link ThreadLocal} variables. This 
+ * may be useful if the library is used in a web service or servlet container.
  */
 public class ThreadLocals {
 	
 	/**
-	 * Resets all {@link ThreadLocal} variables that are used by the decimal4j
-	 * library. The variables are set to {@code null} to make objects held by
-	 * thread local variables available for garbage collection.
+	 * Removes all values held by {@link ThreadLocal} variables that are used by 
+	 * the decimal4j library. {@link ThreadLocal#remove()} is called on every
+	 * variable to make objects held by those variables available for garbage 
+	 * collection.
 	 */
-	public static void resetAll() {
-		StringConversion.STRING_BUILDER_THREAD_LOCAL.set(null);
-		UnsignedDecimal9i36f.THREAD_LOCAL_1.set(null);
-		UnsignedDecimal9i36f.THREAD_LOCAL_2.set(null);
+	public static void removeAll() {
+		StringConversion.STRING_BUILDER_THREAD_LOCAL.remove();
+		UnsignedDecimal9i36f.THREAD_LOCAL_1.remove();
+		UnsignedDecimal9i36f.THREAD_LOCAL_2.remove();
 	}
 	
 	// no instances
