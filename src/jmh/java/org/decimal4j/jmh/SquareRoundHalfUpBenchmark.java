311696066bb1e6802cbf91d076c7b55bf87931a6
Marco Terzer <terzerm@gmail.com>
2015-05-02 23:41:19 +1000

    added separate benchmark for default rounding mode methods without a RoundingMode param

---
+++ src/jmh/java/org/decimal4j/jmh/SquareRoundHalfUpBenchmark.java
@@ -0,0 +1,69 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 decimal4j (tools4j), Marco Terzer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.decimal4j.jmh;
+
+import java.io.IOException;
+
+import org.decimal4j.api.Decimal;
+import org.decimal4j.jmh.state.SquareRoundHalfUpBenchmarkState;
+import org.decimal4j.jmh.state.Values;
+import org.decimal4j.scale.ScaleMetrics;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.RunnerException;
+
+/**
+ * Micro benchmarks for unchecked square.
+ */
+public class SquareRoundHalfUpBenchmark extends AbstractBenchmark {
+
+	@Benchmark
+	@OperationsPerInvocation(OPERATIONS_PER_INVOCATION)
+	public final void immutableDecimals(SquareRoundHalfUpBenchmarkState state, Blackhole blackhole) {
+		for (int i = 0; i < OPERATIONS_PER_INVOCATION; i++) {
+			blackhole.consume(immutableDecimals(state, state.values[i]));
+		}
+	}
+
+	@Benchmark
+	@OperationsPerInvocation(OPERATIONS_PER_INVOCATION)
+	public final void mutableDecimals(SquareRoundHalfUpBenchmarkState state, Blackhole blackhole) {
+		for (int i = 0; i < OPERATIONS_PER_INVOCATION; i++) {
+			blackhole.consume(mutableDecimals(state, state.values[i]));
+		}
+	}
+
+	private static final <S extends ScaleMetrics> Decimal<S> immutableDecimals(SquareRoundHalfUpBenchmarkState state, Values<S> values) {
+		return values.immutable1.square();
+	}
+
+	private static final <S extends ScaleMetrics> Decimal<S> mutableDecimals(SquareRoundHalfUpBenchmarkState state, Values<S> values) {
+		return values.mutable.square();
+	}
+
+	public static void main(String[] args) throws RunnerException, IOException, InterruptedException {
+		run(SquareRoundHalfUpBenchmark.class);
+	}
+}
---
+++ src/jmh/java/org/decimal4j/jmh/state/MultiplyRoundHalfUpBenchmarkState.java
@@ -0,0 +1,45 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 decimal4j (tools4j), Marco Terzer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.decimal4j.jmh.state;
+
+import java.math.RoundingMode;
+
+import org.decimal4j.jmh.value.BenchmarkType;
+import org.decimal4j.jmh.value.ValueType;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class MultiplyRoundHalfUpBenchmarkState extends AbstractValueBenchmarkState {
+	@Param({"Int", "Long"})
+	public ValueType valueType1;
+	@Param({"Int", "Long"})
+	public ValueType valueType2;
+	@Setup
+	public void init() {
+		initForBinaryOp(BenchmarkType.Multiply, RoundingMode.HALF_UP, valueType1, valueType2);
+	}
+}
