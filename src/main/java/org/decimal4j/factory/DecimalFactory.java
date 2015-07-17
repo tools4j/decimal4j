54cb1942d5390d5e522208ed8ea7f182cd5c16b2
Marco Terzer <terzerm@gmail.com>
2015-07-18 00:48:38 +1000

    javadoc for DecimalFactory

--- src/main/java/org/decimal4j/factory/DecimalFactory.java
+++ src/main/java/org/decimal4j/factory/DecimalFactory.java
@@ -33,34 +33,34 @@ import org.decimal4j.api.MutableDecimal;
 import org.decimal4j.scale.ScaleMetrics;
 
 /**
- * Factory for {@link Decimal} values of the scale defined by {@code <S>} and
- * arrays thereof.
+ * Factory for {@link Decimal} values and Decimal arrays of the scale defined by
+ * {@code <S>}.
  *
  * @param <S>
- *            the scale metrics type associated with decimals created by this
- *            factory
+ *            the {@link #getScaleMetrics() scale metrics} type associated with
+ *            decimals created by this factory
  */
 public interface DecimalFactory<S extends ScaleMetrics> {
 	/**
-	 * Returns the scale metrics type associated with decimals created by this
-	 * factory.
+	 * Returns the scale metrics type associated with Decimal values created by
+	 * this factory.
 	 * 
-	 * @return the scale metrics defining the scale for decimals created by this
-	 *         factory
+	 * @return the scale metrics defining the scale for Decimal values created
+	 *         by this factory
 	 */
 	S getScaleMetrics();
 
 	/**
 	 * Returns the implementing class for immutable values.
 	 * 
-	 * @return the implementation type for immutable decimals
+	 * @return the implementation type for immutable Decimal values
 	 */
 	Class<? extends ImmutableDecimal<S>> immutableType();
 
 	/**
 	 * Returns the implementing class for mutable values.
 	 * 
-	 * @return the implementation type for mutable decimals
+	 * @return the implementation type for mutable Decimal values
 	 */
 	Class<? extends MutableDecimal<S>> mutableType();
 
@@ -79,7 +79,8 @@ public interface DecimalFactory<S extends ScaleMetrics> {
 	 * @param scaleMetrics
 	 *            the metrics defining the scale of the Decimal numbers created
 	 *            by the returned factory
-	 * @param <S> the generic type for {@code scaleMetrics}
+	 * @param <S>
+	 *            the generic type for {@code scaleMetrics}
 	 * @return a decimal factory for numbers with the scale specified by
 	 *         {@code scaleMetrics}
 	 */
@@ -87,81 +88,308 @@ public interface DecimalFactory<S extends ScaleMetrics> {
 	<S extends ScaleMetrics> DecimalFactory<S> deriveFactory(S scaleMetrics);
 
 	/**
-	 * Returns a new decimal value whose value is numerically equal to that of
-	 * the specified {@code long} value.
+	 * Returns a new immutable Decimal value whose value is numerically equal to
+	 * that of the specified {@code long} value. An exception is thrown if the
+	 * specified value is too large to be represented as a Decimal of this
+	 * factory's scale.
 	 *
 	 * @param value
-	 *            long value to convert into a decimal
-	 * @return a decimal numerically equal to the specified long value
+	 *            long value to convert into an immutable Decimal value
+	 * @return a Decimal value numerically equal to the specified {@code long}
+	 *         value
+	 * @throws IllegalArgumentException
+	 *             if {@code value} is too large to be represented as a Decimal
+	 *             with the scale of this factory
 	 */
 	ImmutableDecimal<S> valueOf(long value);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@code float} argument to the scale of this
+	 * factory using {@link RoundingMode#HALF_UP} rounding. An exception is
+	 * thrown if the specified value is too large to be represented as a Decimal
+	 * of this factory's scale.
+	 *
+	 * @param value
+	 *            float value to convert into an immutable Decimal value
+	 * @return a Decimal calculated as: <tt>round<sub>HALF_UP</sub>(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} is NaN or infinite or if the magnitude is
+	 *             too large for the float to be represented as a
+	 *             {@code Decimal} with the scale of this factory
+	 */
 	ImmutableDecimal<S> valueOf(float value);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@code float} argument to the scale of this
+	 * factory using the specified {@code roundingMode}. An exception is thrown
+	 * if the specified value is too large to be represented as a Decimal of
+	 * this factory's scale.
+	 *
+	 * @param value
+	 *            float value to convert into an immutable Decimal value
+	 * @param roundingMode
+	 *            the rounding mode to apply during the conversion if necessary
+	 * @return a Decimal calculated as: <tt>round(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} is NaN or infinite or if the magnitude is
+	 *             too large for the float to be represented as a
+	 *             {@code Decimal} with the scale of this factory
+	 * @throws ArithmeticException
+	 *             if {@code roundingMode==UNNECESSARY} and rounding is
+	 *             necessary
+	 */
 	ImmutableDecimal<S> valueOf(float value, RoundingMode roundingMode);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@code double} argument to the scale of this
+	 * factory using {@link RoundingMode#HALF_UP} rounding. An exception is
+	 * thrown if the specified value is too large to be represented as a Decimal
+	 * of this factory's scale.
+	 *
+	 * @param value
+	 *            double value to convert into an immutable Decimal value
+	 * @return a Decimal calculated as: <tt>round<sub>HALF_UP</sub>(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} is NaN or infinite or if the magnitude is
+	 *             too large for the double to be represented as a
+	 *             {@code Decimal} with the scale of this factory
+	 */
 	ImmutableDecimal<S> valueOf(double value);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@code double} argument to the scale of this
+	 * factory using the specified {@code roundingMode}. An exception is thrown
+	 * if the specified value is too large to be represented as a Decimal of
+	 * this factory's scale.
+	 *
+	 * @param value
+	 *            double value to convert into an immutable Decimal value
+	 * @param roundingMode
+	 *            the rounding mode to apply during the conversion if necessary
+	 * @return a Decimal calculated as: <tt>round(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} is NaN or infinite or if the magnitude is
+	 *             too large for the double to be represented as a
+	 *             {@code Decimal} with the scale of this factory
+	 * @throws ArithmeticException
+	 *             if {@code roundingMode==UNNECESSARY} and rounding is
+	 *             necessary
+	 */
 	ImmutableDecimal<S> valueOf(double value, RoundingMode roundingMode);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is numerically equal to
+	 * that of the specified {@link BigInteger} value. An exception is thrown if
+	 * the specified value is too large to be represented as a Decimal of this
+	 * factory's scale.
+	 *
+	 * @param value
+	 *            {@code BigInteger} value to convert into an immutable Decimal
+	 *            value
+	 * @return a Decimal value numerically equal to the specified big integer
+	 *         value
+	 * @throws IllegalArgumentException
+	 *             if {@code value} is too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 */
 	ImmutableDecimal<S> valueOf(BigInteger value);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@link BigDecimal} argument to the scale of this
+	 * factory using {@link RoundingMode#HALF_UP} rounding. An exception is
+	 * thrown if the specified value is too large to be represented as a Decimal
+	 * of this factory's scale.
+	 *
+	 * @param value
+	 *            {@code BigDecimal} value to convert into an immutable Decimal
+	 *            value
+	 * @return a Decimal calculated as: <tt>round<sub>HALF_UP</sub>(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 */
 	ImmutableDecimal<S> valueOf(BigDecimal value);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@link BigDecimal} argument to the scale of this
+	 * factory using the specified {@code roundingMode}. An exception is thrown
+	 * if the specified value is too large to be represented as a Decimal of
+	 * this factory's scale.
+	 *
+	 * @param value
+	 *            {@code BigDecimal} value to convert into an immutable Decimal
+	 *            value
+	 * @param roundingMode
+	 *            the rounding mode to apply during the conversion if necessary
+	 * @return a Decimal calculated as: <tt>round(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 * @throws ArithmeticException
+	 *             if {@code roundingMode==UNNECESSARY} and rounding is
+	 *             necessary
+	 */
 	ImmutableDecimal<S> valueOf(BigDecimal value, RoundingMode roundingMode);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@link Decimal} argument to the scale of this
+	 * factory using {@link RoundingMode#HALF_UP} rounding. An exception is
+	 * thrown if the specified value is too large to be represented as a Decimal
+	 * of this factory's scale.
+	 *
+	 * @param value
+	 *            Decimal value to convert into an immutable Decimal value of
+	 *            this factory's scale
+	 * @return a Decimal calculated as: <tt>round<sub>HALF_UP</sub>(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 */
 	ImmutableDecimal<S> valueOf(Decimal<?> value);
 
+	/**
+	 * Returns a new immutable Decimal value whose value is calculated by
+	 * rounding the specified {@link Decimal} argument to the scale of this
+	 * factory using the specified {@code roundingMode}. An exception is thrown
+	 * if the specified value is too large to be represented as a Decimal of
+	 * this factory's scale.
+	 *
+	 * @param value
+	 *            Decimal value to convert into an immutable Decimal value of
+	 *            this factory's scale
+	 * @param roundingMode
+	 *            the rounding mode to apply during the conversion if necessary
+	 * @return a Decimal calculated as: <tt>round(value)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 * @throws ArithmeticException
+	 *             if {@code roundingMode==UNNECESSARY} and rounding is
+	 *             necessary
+	 */
 	ImmutableDecimal<S> valueOf(Decimal<?> value, RoundingMode roundingMode);
 
+	/**
+	 * Translates the string representation of a {@code Decimal} into an
+	 * immutable {@code Decimal}. The string representation consists of an
+	 * optional sign, {@code '+'} (<tt> '&#92;u002B'</tt>) or {@code '-'} (
+	 * <tt>'&#92;u002D'</tt>), followed by a sequence of zero or more decimal
+	 * digits ("the integer"), optionally followed by a fraction.
+	 * <p>
+	 * The fraction consists of a decimal point followed by zero or more decimal
+	 * digits. The string must contain at least one digit in either the integer
+	 * or the fraction. If the fraction contains more digits than this factory's
+	 * scale, the value is rounded using {@link RoundingMode#HALF_UP HALF_UP}
+	 * rounding. An exception is thrown if the value is too large to be
+	 * represented as a Decimal of this factory's scale.
+	 *
+	 * @param value
+	 *            String value to convert into an immutable Decimal value of
+	 *            this factory's scale
+	 * @return a Decimal calculated as: <tt>round<sub>HALF_UP</sub>(value)</tt>
+	 * @throws NumberFormatException
+	 *             if {@code value} does not represent a valid {@code Decimal}
+	 *             or if the value is too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 */
 	ImmutableDecimal<S> parse(String value);
 
+	/**
+	 * Translates the string representation of a {@code Decimal} into an
+	 * immutable {@code Decimal}. The string representation consists of an
+	 * optional sign, {@code '+'} (<tt> '&#92;u002B'</tt>) or {@code '-'} (
+	 * <tt>'&#92;u002D'</tt>), followed by a sequence of zero or more decimal
+	 * digits ("the integer"), optionally followed by a fraction.
+	 * <p>
+	 * The fraction consists of a decimal point followed by zero or more decimal
+	 * digits. The string must contain at least one digit in either the integer
+	 * or the fraction. If the fraction contains more digits than this factory's
+	 * scale, the value is rounded using the specified {@code roundingMode}. An
+	 * exception is thrown if the value is too large to be represented as a
+	 * Decimal of this factory's scale.
+	 *
+	 * @param value
+	 *            String value to convert into an immutable Decimal value of
+	 *            this factory's scale
+	 * @param roundingMode
+	 *            the rounding mode to apply if the fraction contains more
+	 *            digits than the scale of this factory
+	 * @return a Decimal calculated as: <tt>round(value)</tt>
+	 * @throws NumberFormatException
+	 *             if {@code value} does not represent a valid {@code Decimal}
+	 *             or if the value is too large to be represented as a Decimal
+	 *             with the scale of this factory
+	 * @throws ArithmeticException
+	 *             if {@code roundingMode==UNNECESSARY} and rounding is
+	 *             necessary
+	 */
 	ImmutableDecimal<S> parse(String value, RoundingMode roundingMode);
 
 	/**
-	 * Creates and returns an immutable value from an unscaled long value.
+	 * Returns a new immutable Decimal value whose value is numerically equal to
+	 * <tt>(unscaled &times; 10<sup>-scale</sup>)</tt> where {@code scale}
+	 * refers to this factory's scale.
 	 *
 	 * @param unscaled
-	 *            the unscaled long value
-	 * @return an immutable value.
+	 *            unscaled value to convert into an immutable Decimal value
+	 * @return a Decimal calculated as:
+	 *         <tt>unscaled &times; 10<sup>-scale</sup></tt>
 	 */
 	ImmutableDecimal<S> valueOfUnscaled(long unscaled);
 
 	/**
-	 * Returns a new immutable decimal value from the specified unscaled value
-	 * with the given scale. If the scale conversion leads to rounding
-	 * {@link RoundingMode#HALF_EVEN HALF_EVEN} rounding mode is applied;
-	 * potential overflows are silently truncated.
-	 * 
-	 * @param unscaledValue
-	 *            the unscaled value to convert
+	 * Returns a new immutable Decimal value whose value is numerically equal to
+	 * <tt>(unscaled &times; 10<sup>-scale</sup>)</tt>. The result is rounded to
+	 * the scale of this factory using {@link RoundingMode#HALF_UP HALF_UP}
+	 * rounding. An exception is thrown if the specified value is too large to
+	 * be represented as a Decimal of this factory's scale.
+	 *
+	 * @param unscaled
+	 *            unscaled value to convert into an immutable Decimal value
 	 * @param scale
-	 *            the scale used for {@code unscaledValue}
-	 * @return a new immutable decimal value converted from the specified
-	 *         unscaled value with the given scale
+	 *            the scale to apply to the {@code unscaled} value
+	 * @return a Decimal calculated as:
+	 *         <tt>round<sub>HALF_UP</sub>(unscaled &times; 10<sup>-scale</sup>)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} too large to be represented as a Decimal
+	 *             of this factory's scale
 	 */
-	ImmutableDecimal<S> valueOfUnscaled(long unscaledValue, int scale);
+	ImmutableDecimal<S> valueOfUnscaled(long unscaled, int scale);
 
 	/**
-	 * Returns a new immutable decimal value from the specified unscaled value
-	 * with the given scale. If the scale conversion leads to rounding the
-	 * specified rounding mode is applied; potential overflows are silently
-	 * truncated.
-	 * 
-	 * @param unscaledValue
-	 *            the unscaled value to convert
+	 * Returns a new immutable Decimal value whose value is numerically equal to
+	 * <tt>(unscaled &times; 10<sup>-scale</sup>)</tt>. The result is rounded to
+	 * the scale of this factory using the specified {@code roundingMode}. An
+	 * exception is thrown if the specified value is too large to be represented
+	 * as a Decimal of this factory's scale.
+	 *
+	 * @param unscaled
+	 *            unscaled value to convert into an immutable Decimal value
 	 * @param scale
-	 *            the scale used for {@code unscaledValue}
+	 *            the scale to apply to the {@code unscaled} value
 	 * @param roundingMode
-	 *            the rounding mode to apply if the conversion leads to rounding
-	 * @return a new immutable decimal value converted from the specified
-	 *         unscaled value with the given scale
+	 *            the rounding mode to apply during the conversion if necessary
+	 * @return a Decimal calculated as:
+	 *         <tt>round(unscaled &times; 10<sup>-scale</sup>)</tt>
+	 * @throws IllegalArgumentException
+	 *             if {@code value} too large to be represented as a Decimal
+	 *             of this factory's scale
+	 * @throws ArithmeticException
+	 *             if {@code roundingMode==UNNECESSARY} and rounding is
+	 *             necessary
 	 */
-	ImmutableDecimal<S> valueOfUnscaled(long unscaledValue, int scale, RoundingMode roundingMode);
+	ImmutableDecimal<S> valueOfUnscaled(long unscaled, int scale, RoundingMode roundingMode);
 
 	/**
 	 * Creates a one dimensional array of the specified {@code length} for
-	 * immutable decimal values.
+	 * immutable Decimal values.
 	 * 
 	 * @param length
 	 *            the length of the returned array
@@ -172,13 +400,13 @@ public interface DecimalFactory<S extends ScaleMetrics> {
 	/**
 	 * Creates a new mutable value initialized with zero.
 	 * 
-	 * @return a new mutable decimal value representing zero.
+	 * @return a new mutable Decimal value representing zero.
 	 */
 	MutableDecimal<S> newMutable();
 
 	/**
 	 * Creates a one dimensional array of the specified {@code length} for
-	 * mutable decimal values.
+	 * mutable Decimal values.
 	 * 
 	 * @param length
 	 *            the length of the returned array
