c60379c711a7ef585db92a70a4813c304d4e2adc
Marco Terzer <terzerm@gmail.com>
2021-03-14 00:05:40 +1100

    Update year in license; format license header in multiline comment style instead of javadoc

--- src/main/java/org/decimal4j/util/DoubleRounder.java
+++ src/main/java/org/decimal4j/util/DoubleRounder.java
@@ -1,7 +1,7 @@
-/**
+/*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015-2020 decimal4j (tools4j), Marco Terzer
+ * Copyright (c) 2015-2021 decimal4j (tools4j), Marco Terzer
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
